9942c4b814d76c1d83ef29ae9d50500bfd7849eb
appuser <appuser@journey2-679f96f5bd-hjzbt>
2024-07-17 11:05:15 +0000

    starter project

---
+++ client/src/main.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Layout from "./Pages/Layout";
+import ErrorPage from "./Pages/ErrorPage";
+import EmployeeList from "./Pages/EmployeeList";
+import EmployeeCreator from "./Pages/EmployeeCreator";
+import EmployeeUpdater from "./Pages/EmployeeUpdater";
+
+import "./main.css";
+import TableTest from "./Pages/TableTest";
+import FormTest from "./Pages/FormTest";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <EmployeeList />,
+      },
+      {
+        path: "/create",
+        element: <EmployeeCreator />,
+      },
+      {
+        path: "/update/:id",
+        element: <EmployeeUpdater />,
+      },
+      {
+        path: "/table-test",
+        element: <TableTest />,
+      },
+      {
+        path: "/form-test",
+        element: <FormTest />,
+      },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
