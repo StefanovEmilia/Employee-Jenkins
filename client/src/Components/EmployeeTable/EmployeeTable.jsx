904535c5fee34a759e855c2f52e8f0ada55450f9
Stefanov Emilia <stemilia92@gmail.com>
2024-07-17 17:22:28 +0300

    rearrange by

--- client/src/Components/EmployeeTable/EmployeeTable.jsx
+++ client/src/Components/EmployeeTable/EmployeeTable.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import "./EmployeeTable.css";
 
-const EmployeeTable = ({ employees, onDelete, setFilteredEmployees }) => {
+const EmployeeTable = ({ employees, onDelete, setFilteredEmployees, setOrderBy }) => {
 
   const handleInput = (e) => {
     if (e.target.value === '') {
@@ -24,7 +24,16 @@ const EmployeeTable = ({ employees, onDelete, setFilteredEmployees }) => {
             <th>Name</th>
             <th>Level <input type="text" name="level" placeholder="filter by level" onChange={handleInput} /></th>
             <th>Position <input type="text" name="position" placeholder="filter by position" onChange={handleInput} /></th>
-            <th />
+            <th>Order by:
+              <select onChange={(e) => { setOrderBy(e.target.value) }}>
+                <option>Select order</option>
+                <option value="name">Name</option>
+                <option value="first">First Name</option>
+                <option value="last">Last Name</option>
+                <option value="middle">Middle Name</option>
+                <option value="position">Position</option>
+                <option value="level">Level</option>
+            </select></th>
           </tr>
         </thead>
         <tbody>
--- client/src/Pages/EmployeeList.jsx
+++ client/src/Pages/EmployeeList.jsx
@@ -16,6 +16,7 @@ const EmployeeList = () => {
   const [loading, setLoading] = useState(true);
   const [employees, setEmployees] = useState(null);
   const [filteredEmployees, setFilteredEmployees] = useState(null)
+  const [orderBy, setOrderBy] = useState(null)
 
   const handleDelete = (id) => {
     deleteEmployee(id);
@@ -33,12 +34,81 @@ const EmployeeList = () => {
       })
   }, []);
 
+  useEffect(() => {
+    if (orderBy) {
+      const list = filteredEmployees ? [...filteredEmployees] : [...employees];
+      let ordered = [];
+
+      if (orderBy === "first" || orderBy === "last" || orderBy === "middle") {
+        if (orderBy === "first" || orderBy === "last") {
+          ordered = list.sort((a, b) => {
+            const indexA = orderBy === "first" ? 0 : a.name.split(" ").length - 1;
+            const indexB = orderBy === "first" ? 0 : b.name.split(" ").length - 1;
+            if (a.name.split(" ")[indexA] < b.name.split(" ")[indexB]) {
+              return -1;
+            }
+            if (a.name.split(" ")[indexA] > b.name.split(" ")[indexB]) {
+              return 1;
+            }
+            return 0;
+          })
+        } else {
+          const middles = list.filter(employee => employee.name.split(' ').length > 2).sort((a, b) => {
+            if (a.name.split(' ')[1] < b.name.split(' ')[1]) {
+              return -1
+            }
+            if (a.name.split(' ')[1] > b.name.split(' ')[1]) {
+              return 1
+            }
+            return 0
+          })
+          const normals = list.filter(employee => employee.name.split(' ').length === 2).sort((a, b) => {
+            if (a.name < b.name) {
+              return -1
+            }
+            if (a.name > b.name) {
+              return 1
+            }
+            return 0
+          })
+          ordered = [...middles, ...normals]
+          console.log(middles)
+        }
+      } else {
+        ordered = list.sort((a, b) => {
+          if (a[orderBy] < b[orderBy]) {
+            return -1
+          }
+          if (a[orderBy] > b[orderBy]) {
+            return 1
+          }
+          return 0
+        })
+      }
+      
+      if (filteredEmployees) {
+        if (ordered.length > 0) {
+          setFilteredEmployees(ordered);
+        }
+      } else {
+        if (ordered.length > 0) {
+          setEmployees(ordered);
+        }
+      }
+    }
+  }, [orderBy])
+
   if (loading) {
     return <Loading />;
   }
 
   return ( 
-      <EmployeeTable employees={filteredEmployees? filteredEmployees : employees} onDelete={handleDelete} setFilteredEmployees={setFilteredEmployees}/> 
+    <EmployeeTable
+      employees={filteredEmployees ? filteredEmployees : employees}
+      onDelete={handleDelete}
+      setFilteredEmployees={setFilteredEmployees}
+      setOrderBy={setOrderBy}
+    /> 
   );
 };
 
