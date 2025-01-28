5dab87d11eada81b9c9569a9f5d20a4eb849052c
Stefanov Emilia <stemilia92@gmail.com>
2025-01-28 11:52:19 +0300

    change server port to 8081

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ import dotenv from "dotenv"
 
 dotenv.config()
 
-const { MONGO_URL, PORT = 8080 } = process.env;
+const { MONGO_URL, PORT = 8081 } = process.env;
 
 if (!MONGO_URL) {
   console.error("Missing MONGO_URL environment variable");
@@ -24,7 +24,7 @@ const main = async () => {
   await connect(MONGO_URL);
 
   app.listen(PORT, () => {
-    console.log("App is listening on 8080");
+    console.log("App is listening on 8081");
     console.log("Try /api/employees route right now");
   });
 };
